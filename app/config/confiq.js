8fcc13d7089045e99687d508f8f22341f6ef67bc
rikian <rikianfaisal@gmail.com>
2022-07-22 06:47:02 +0700

    h

--- app/config/confiq.js
+++ app/config/confiq.js
@@ -1,6 +1,6 @@
 const address = "127.0.0.1"
 const port = 9091
-// const host = `https://tes-praktek-node-js-progammer.herokuapp.com`
-const host = `http://localhost:9091`
+const host = `https://tes-praktek-node-js-progammer.herokuapp.com`
+// const host = `http://localhost:9091`
 
 module.exports = { host, port, address }
