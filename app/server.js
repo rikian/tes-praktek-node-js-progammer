300686d95fae3238d951c9880d4ffdb785596e10
rikian <rikianfaisal@gmail.com>
2022-07-22 00:54:32 +0700

    test node js

--- app/server.js
+++ app/server.js
@@ -6,15 +6,15 @@ const { router } = require("./router/router");
 
 function run(port, address) {
     app.get("/", (req, res) => res.send("hello"))
-    // app.use(cookieParser())
-    // app.set("view engine", "ejs");
-    // app.use("/public/", express.static("static"));
-    // app.use("/plugins/", express.static("static/adminlte/plugins"));
-    // app.use("/dist/", express.static("static/adminlte/dist"));
-    // app.use("/media/", express.static("media"));
-    // app.use((req, res, next) => middleware(req, res, next))
-    // app.use("/", router)
-    app.listen(process.env.PORT || 9091, () => console.log("server listening on port " + port)).on("error", (err) => console.log(`failed listen at 127.0.0.0:${port}. Error : ${err.message}`))
+    app.use(cookieParser())
+    app.set("view engine", "ejs");
+    app.use("/public/", express.static("static"));
+    app.use("/plugins/", express.static("static/adminlte/plugins"));
+    app.use("/dist/", express.static("static/adminlte/dist"));
+    app.use("/media/", express.static("media"));
+    app.use((req, res, next) => middleware(req, res, next))
+    app.use("/", router)
+    app.listen(process.env.PORT || port, address, () => console.log("server listening on port " + port)).on("error", (err) => console.log(`failed listen at 127.0.0.0:${port}. Error : ${err.message}`))
 }
 
 module.exports = { run }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const { run } = require("./app/server")
 const { port, address } = require("./app/config/confiq")
 
-run(9091)
+run(port, address)
